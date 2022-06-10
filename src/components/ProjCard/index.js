1333ce4b3d7c51c15d513da302aad6925d260477
nick quinn <nick.quinn@stadium1.com>
2022-06-10 08:44:38 -0400

    added the rest of my work from my github

--- src/components/ProjCard/index.js
+++ src/components/ProjCard/index.js
@@ -11,43 +11,42 @@ const ProjCard = (props) => {
         <Card body>
           <CardTitle>Find a Flick</CardTitle>
           <CardText>Find a flick, click a movie genre and get a title and trailer for that genre!</CardText>
-          <Button href = 'https://github.com/kaitdiaz01/Find-A-Flick'>GitHub Repo</Button>
           <Button href = 'https://kaitdiaz01.github.io/Find-A-Flick/'>Find-A-Flick</Button>
         </Card>
       </Col>
       <Col sm="6">
         <Card body>
           <CardTitle>Snack Movers</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <CardText>With supporting text below as a natural lead-in to additional content!</CardText>
           <Button href= ' https://fast-tundra-37022.herokuapp.com/'>SNACK Movers</Button>
         </Card>
       </Col>
       <Col sm="6">
         <Card body>
-          <CardTitle>Text Editor</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
+          <CardTitle>Password Generator</CardTitle>
+          <CardText>Generate a password for your login!</CardText>
+          <Button href = 'https://nquinn95.github.io/password-generator/'>Check it out!</Button>
         </Card>
       </Col>
       <Col sm="6">
         <Card body>
-          <CardTitle></CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
+          <CardTitle>Employee Managemnt System</CardTitle>
+          <CardText>Created an Employee Managemnt System to add employees, track salaries and more!</CardText>
+          <Button href = 'https://drive.google.com/file/d/13W6vdhSt3FKXdrXc5FlN8CcXoFHZzCzl/view'>Checkout a video of how it works!</Button>
         </Card>
       </Col>
       <Col sm="6">
         <Card body>
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
+          <CardTitle>The First Thing I ever did!</CardTitle>
+          <CardText>Everybody has to start somewhere!</CardText>
+          <Button href = 'https://nquinn95.github.io/code-refractor/'>Check it out!</Button>
         </Card>
       </Col>
       <Col sm="6">
         <Card body>
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
+          <CardTitle>First portfolio</CardTitle>
+          <CardText>My very first portfolio!</CardText>
+          <Button href = 'https://nquinn95.github.io/nick-quinn-portfolio/'>Check it out</Button>
         </Card>
       </Col>
     </Row>
